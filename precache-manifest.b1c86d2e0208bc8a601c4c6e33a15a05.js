e8fc284740b502c3d37a921c5a9293c3787b0f65
dianamaciag <diana.maciag92@gmail.com>
2019-02-26 09:53:05 +0100

    Updates

---
+++ precache-manifest.b1c86d2e0208bc8a601c4c6e33a15a05.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3da6564f4aa20c4e655f",
+    "url": "/moje-rachunki/static/js/runtime~main.3da6564f.js"
+  },
+  {
+    "revision": "10173b30e59360545bfb",
+    "url": "/moje-rachunki/static/js/main.10173b30.chunk.js"
+  },
+  {
+    "revision": "4624aeddba04f0e21f04",
+    "url": "/moje-rachunki/static/js/1.4624aedd.chunk.js"
+  },
+  {
+    "revision": "10173b30e59360545bfb",
+    "url": "/moje-rachunki/static/css/main.5e9ded00.chunk.css"
+  },
+  {
+    "revision": "22a4a69386332e83ff5fc1f4ada7550f",
+    "url": "/moje-rachunki/index.html"
+  }
+];
